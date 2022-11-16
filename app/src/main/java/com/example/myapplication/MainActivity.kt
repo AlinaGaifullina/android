179beeb0c2c276d4ffe16ae16d613cbd9e2bb249
AlinaGaifullina <kivi2177@gmail.com>
2022-11-16 21:22:03 +0300

    Оно работает!!

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val containerID = R.id.container
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(savedInstanceState !=null) {
+            return
+        }
+        supportFragmentManager.beginTransaction()
+            .add(
+                containerID,
+                MainFragment.newInstance(Bundle()),
+                MainFragment.MAIN_FRAGMENT_TAG
+            ).commit()
+    }
+}
